5613e0bb73cd9232b38b0e9bcff08ecb5c9deffe
ricardogh <rgherdt@inf.ufrgs.br>
2014-01-30 16:17:52 -0200

    translated from Mappings to Morphism
    removed the temporaly added (simpler) structure Mapping to handle
    proper Morphisms.

--- src/Graph/Match.hs
+++ src/Graph/Match.hs
@@ -24,13 +24,11 @@ findMatches l g = undefined
 matchEdge :: TypedDigraph a b -> TypedDigraph a b -> Int -> Morphism a b -> [Morphism a b]
 matchEdge = undefined
 
-type Mapping = [(Int, Int)]
-
-{- | A Condition consists of a function that, given a Mapping 'p', two
+{- | A Condition consists of a function that, given a Morphism 'm', two
 TypedDigraphs 'l', 'g' and two Edges 'le', 'ge', checks if 'ge' satisfies it
 -}
 type Condition a b =
-	Mapping 
+	Morphism (TypeInfo a) b
 	-> TypedDigraph a b
 	-> Edge b 
 	-> TypedDigraph a b
@@ -39,7 +37,7 @@ type Condition a b =
 
 {- | checks if 'le' and 'ge' have source nodes from same type -}
 srcTypeCond :: Condition a b
-srcTypeCond p l le g ge =
+srcTypeCond m l le g ge =
 	ltype == gtype
 	where
 		ltype = srcType le l
@@ -47,78 +45,90 @@ srcTypeCond p l le g ge =
 
 {- | checks if 'le' and 'ge' have target nodes from same type -}
 tarTypeCond :: Condition a b
-tarTypeCond p l le g ge =
+tarTypeCond m l le g ge =
 	ltype == gtype
 	where
 		ltype = tarType le l
 		gtype = tarType ge g
 
-{- | figures out if 'le's source already occurs in 'p'. If that's the case,
+{- | figures out if 'le's source already occurs in 'm'. If that's the case,
 srcIDCond checks if 'ge's source is the same node to which 'le's source got
-mapped.  If so, 'ge' is a matching Edge. If 'le's source doesn't occur in 'p',
+mapped.  If so, 'ge' is a matching Edge. If 'le's source doesn't occur in 'm',
 any 'ge' will satisfy this condition
 -}
 srcIDCond :: Condition a b
-srcIDCond p l le g ge =
-	let lsrc = source le
-	    gsrc = source ge
-	    matched = (\(ln, gn) -> ln == lsrc) `L.find` p
+srcIDCond m@(Morphism nal _) l le g ge =
+	let lsrc = sourceID le
+	    gsrc = sourceID ge
+	    matched = (\na ->
+		case na of
+			(Just n, _) -> lsrc == nodeID n
+			otherwise -> False
+			) `L.find` nal
 	in case matched of	
-		Just (ln, gn) -> gsrc == gn
-		Nothing -> True
+		Just (_, Just n) -> gsrc == nodeID n
+		otherwise -> True
 
-{- | figures out if 'le's target already occurs in 'p'. If that's the case,
+{- | figures out if 'le's target already occurs in 'm'. If that's the case,
 tarIDCond checks if 'ge's target is the same node to which 'le's target got
-mapped.  If so, 'ge' is a matching Edge. If 'le's target doesn't occur in 'p',
+mapped.  If so, 'ge' is a matching Edge. If 'le's target doesn't occur in 'm',
 any 'ge' will satisfy this condition
 -}
 tarIDCond :: Condition a b
-tarIDCond p l le g ge =
-	let ltar = target le
-	    gtar = target ge
-            matched = (\(ln, gn) -> ln == ltar) `L.find` p
+tarIDCond m@(Morphism nal _) l le g ge =
+	let ltar = targetID le
+	    gtar = targetID ge
+	    matched = (\na ->
+		case na of
+			(Just n, _) -> ltar == nodeID n
+			otherwise -> False
+			) `L.find` nal
 	in case matched of	
-		Just (ln, gn) -> gtar == gn
-		Nothing -> True
+		Just (_, Just n) -> gtar == nodeID n
+		otherwise -> True
+
 
 conditionList = [srcTypeCond, tarTypeCond, srcIDCond, tarIDCond]
 
 {- | if all conditions in 'cl' get satisfied by the given edge 'ge', returns the
-'p' Mapping with the new source/target pairs added. 
+'m' Morphism with the new source/target pairs added. 
 -}
 satisfiesCond
 	:: [Condition a b]
-	-> Mapping
+	-> Morphism (TypeInfo a) b
 	-> TypedDigraph a b
 	-> Edge b
 	-> TypedDigraph a b
 	-> Edge b
-	-> Maybe Mapping
-satisfiesCond cl p l le g ge =
-	if foldr (\c acc -> (c p l le g ge) && acc) True cl 
-	then Just $ (target le, target ge) : (source le, source ge) : p
+	-> Maybe (Morphism (TypeInfo a) b)
+satisfiesCond cl m l@(TypedDigraph ld _) le g@(TypedDigraph gd _) ge =
+	if foldr (\c acc -> (c m l le g ge) && acc) True cl 
+	then Just $
+		addNodeAction (target le ld) (target ge gd)
+		$ addNodeAction (source le ld) (source ge gd)
+		$ addEdgeAction	le ge
+		$ m
 	else Nothing
 
-{- | given a Condition list, a Mapping 'p', two Graphs 'l', 'g' and an Edge
+{- | given a Condition list, a Morphism 'm', two Graphs 'l', 'g' and an Edge
 'le', searches for all edges from graph 'g' that satisfies the conditions in
-this context.  Returns a list of Mappings, each with the new possibility added
+this context.  Returns a list of Morphisms, each with the new possibility added
 -}
 applyCond
 	:: [Condition a b]
-	-> Mapping
+	-> Morphism (TypeInfo a) b
 	-> TypedDigraph a b
 	-> Edge b
 	-> TypedDigraph a b
-	-> [Mapping]
-applyCond cl p l le g@(TypedDigraph dg _) =
-	let candidates = mapMaybe (\ge -> satisfiesCond cl p l le g ge) $ edges dg
+	-> [Morphism (TypeInfo a) b]
+applyCond cl m l le g@(TypedDigraph dg _) =
+	let candidates = mapMaybe (\ge -> satisfiesCond cl m l le g ge) $ edges dg
 	in foldr (\c acc -> c : acc) [] candidates 
 
 {- | written for test purposes in Example.hs.  -}
-testFunc :: Int -> TypedDigraph a b -> TypedDigraph a b -> Maybe [Mapping]
+testFunc :: Int -> TypedDigraph a b -> TypedDigraph a b -> Maybe [Morphism (TypeInfo a) b]
 testFunc id l@(TypedDigraph (Digraph _ em) _) g =
 	let le = IM.lookup id em
-	in
-		case le of
-			Just e -> Just $ applyCond conditionList [] l e g
-			Nothing -> Nothing
+	in case le of
+		Just e -> Just $ applyCond conditionList (Morphism [] []) l e g
+		Nothing -> Nothing
