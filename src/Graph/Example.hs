6f255682e274aa9fe47dff702dbfe9736684ddc2
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2014-03-23 17:15:39 -0300

    Match.hs now works with a simpler representation: Mapping

--- src/Graph/Example.hs
+++ src/Graph/Example.hs
@@ -6,15 +6,15 @@ l = D.empty
 g = D.empty
 tg = D.empty
 
-ln = 	[ D.Node 1 (1, "1")
-	, D.Node 2 (1, "2")
-	, D.Node 3 (2, "3")
-	, D.Node 4 (1, "4")
-	, D.Node 5 (2, "5")
+ln = 	[ D.Node 1 1 "1"
+	, D.Node 2 1 "2"
+	, D.Node 3 2 "3"
+	, D.Node 4 1 "4"
+	, D.Node 5 2 "5"
 	]
-le = 	[ D.Edge 4 (2, 1) (1, "2 -> 1")
-	, D.Edge 5 (1, 3) (1, "1 -> 3")
-	, D.Edge 6 (4, 4) (2, "4 -> 4")
+le = 	[ D.Edge 4 (2, 1) 1 "2 -> 1"
+	, D.Edge 5 (1, 3) 1 "1 -> 3"
+	, D.Edge 6 (4, 4) 2 "4 -> 4"
 	]
 
 l1 = foldM (\d n -> D.addNode n d) l ln :: Maybe (D.Digraph String String)
@@ -22,19 +22,19 @@ l2 = l1 >>=
 	(\l -> foldM (\d e -> D.addEdge e d) l le) :: Maybe (D.Digraph String String)
 
 	
-gn = 	[ D.Node 10 (1, "a")
-	, D.Node 11 (1, "b")
-	, D.Node 12 (1, "c")
-	, D.Node 13 (2, "d")
-	, D.Node 14 (1, "e")
-	, D.Node 15 (2, "f")
-	, D.Node 16 (2, "g")
+gn = 	[ D.Node 10 1 "a"
+	, D.Node 11 1 "b"
+	, D.Node 12 1 "c"
+	, D.Node 13 2 "d"
+	, D.Node 14 1 "e"
+	, D.Node 15 2 "f"
+	, D.Node 16 2 "g"
 	]
-ge = 	[ D.Edge 15 (10, 11) (1, "a -> b")
-	, D.Edge 16 (10, 12) (1, "a -> c")
-	, D.Edge 17 (12, 13) (1, "c -> d")
-	, D.Edge 18 (14, 14) (2, "e -> e")
-	, D.Edge 19 (10, 12) (2, "a -> c")
+ge = 	[ D.Edge 15 (10, 11) 1 "a -> b"
+	, D.Edge 16 (10, 12) 1 "a -> c"
+	, D.Edge 17 (12, 13) 1 "c -> d"
+	, D.Edge 18 (14, 14) 2 "e -> e"
+	, D.Edge 19 (10, 12) 2 "a -> c"
 	]
 g1 = foldM (\d n -> D.addNode n d) g gn :: Maybe (D.Digraph String String)
 g2 = g1 >>=
@@ -47,5 +47,4 @@ tdg = case g2 of
 	Just t -> D.TypedDigraph t tg
 	otherwise -> D.TypedDigraph g tg
 
-
-morphisms = findMatches tdl tdg
+mappings = findMatches tdl tdg
