9432c70bc9ffb5d8e354b7ebbbf1e6962e9e9f60
rgherdt <rgherdt@inf.ufrgs.br>
2014-08-08 12:25:47 -0300

    added minimal documentation

--- src/Graph/MorphismClass.hs
+++ src/Graph/MorphismClass.hs
@@ -4,17 +4,21 @@ import Data.List
 
 class GraphClass g => MorphismClass (m g) where
 
-    domain             :: m g -> g
-    codomain           :: m g -> g
-    mapping            :: m g -> ([(Nd g, Nd g)], [(Ed g, Ed g)])
+    -- Extract essencial contents of a morphism
+    domain             :: m g -> g                                -- required
+    codomain           :: m g -> g                                -- required
+    mapping            :: m g -> ([(Nd g, Nd g)], [(Ed g, Ed g)]) -- required
 
-    empty              :: g -> g -> m g
-    updateNodeMapping  :: (Nd g) -> (Nd g) -> m g -> m g
-    updateEdgeMapping  :: (Ed g) -> (Ed g) -> m g -> m g
+    -- Create and manipulate morphisms
+    empty              :: g -> g -> m g                           -- required
+    updateNodeMapping  :: (Nd g) -> (Nd g) -> m g -> m g          -- required
+    updateEdgeMapping  :: (Ed g) -> (Ed g) -> m g -> m g          -- required
 
+    -- Query functions
     hasNodeMapping     :: Nd g -> Nd g -> m g -> Bool
     hasEdgeMapping     :: Ed g -> Ed g -> m g -> Bool
 
+    -- Query functions: default implementation
     hasNodeMapping lNode rNode m =
         (lNode, rNode) `elem` nodeMappings
       where
